9ddd9f3c34e86878914703d504ffab4b4a971dfe
young.jiandong <young.jiandong@gmail.com>
2010-12-14 21:53:47 +0800

    update js

---
+++ src/main/webapp/WEB-INF/app/controllers/employees_controller.rb
@@ -0,0 +1,31 @@
+# extjs json
+class EmployeesController < Application
+
+  def index
+    start = (params[:start] || 0).to_i
+    size = (params[:limit] || 20).to_i
+    page = ((start/size).to_i) + 1
+    sort = "#{params[:sort]} #{params[:dir]}" || "id asc"
+
+    @employees = Employee.find(:all,
+                               :page => page,
+                               :per_page => size,
+                               :order => sort
+    )
+
+    rtndata = {}
+    rtndata[:total] = @employees.total_entries
+    rtndata[:employees] = @employees.collect {|e| {
+        :id => e.id,
+        :last_name => e.last_name,
+        :first_name => e.first_name,
+        :department => e.department,
+        :designation => e.designation
+      }}
+
+    respond_to do |format|
+      format.json { render :json => rtndata.to_json }
+    end
+  end
+
+end
